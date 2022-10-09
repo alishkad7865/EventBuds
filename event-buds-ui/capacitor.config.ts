c5d36e68803352b7658a20ffe9067ed614546c64
alishkad7865 <alishkadiwal@uregina.ca>
2022-10-09 13:45:17 -0600

    Add Ionic and capacitor and setup for andriod and ios

---
+++ event-buds-ui/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'EventBuds.UI',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
---
+++ event-buds-ui/src/pages/Tab1.tsx
@@ -0,0 +1,26 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Tab1.css';
+
+const Tab1: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Tab 1</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Tab 1</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer name="Tab 1 page" />
+        <p>This is tab 1 and Welcome to the Event bud app</p>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab1;
