5596d6805c65bc59289acf835084db65161d8482
alishkad7865 <alishkadiwal@uregina.ca>
2023-01-09 21:00:56 -0600

    add new package and update xml file for android manifest

--- event-buds-ui/src/api/userApi.tsx
+++ event-buds-ui/src/api/userApi.tsx
@@ -1,18 +1,32 @@
 import axios from "axios";
+import { CapacitorHttp, HttpResponse } from "@capacitor/core";
 export async function getUser(userId: number) {
-  let baseUrl = "http://140.238.138.230:8000/User/getUser?userId=" + userId;
-  return axios
-    .get(`${baseUrl}`, {
-      headers: {
-        Accept: "application/json",
-      },
-    })
+  let baseUrl = "http://140.238.138.230:8000/User/getUser";
+  const options = {
+    url: baseUrl,
+    headers: {},
+    params: { userId: userId.toString() },
+  };
+  const response: HttpResponse = await CapacitorHttp.get(options)
     .then((response: any) => {
       if (response.status >= 200 && response.status < 300) {
-        return response.data;
+        return response;
       }
     })
     .catch((e: any) => console.log(e));
+  return response.data;
+  // return axios
+  //   .get(`${baseUrl}`, {
+  //     headers: {
+  //       Accept: "application/json",
+  //     },
+  //   })
+  //   .then((response: any) => {
+  //     if (response.status >= 200 && response.status < 300) {
+  //       return response.data;
+  //     }
+  //   })
+  //   .catch((e: any) => console.log(e));
 }
 
 export async function getAllUsers(userId: number) {
