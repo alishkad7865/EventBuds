f3d38d525fbeefd2a25b4dda45963ef31e7dffe2
alishkad7865 <alishkadiwal@uregina.ca>
2023-01-26 14:27:42 -0600

    update static info

--- event-buds-api/Model/EventModel.py
+++ event-buds-api/Model/EventModel.py
@@ -3,25 +3,25 @@ from pydantic import BaseModel
 from typing import Optional
 
 
-
 class User(BaseModel):
     userId: int
     firstName: str
     lastName: str
     email: str
     address: Optional[str] = ""
-    sex: Optional[str]= ""
+    sex: Optional[str] = ""
     bio: Optional[str] = ""
     isActive: Optional[int] = 1
     friends: Optional[list] = []
     userRowId: Optional[int] = None
+
 
 class Event(BaseModel):
     eventId: Optional[int] = None
     createdBy: str
     ownerId: int
     eventTitle: str
-    eventRegEndDateTime: Optional[datetime]= None
+    eventRegEndDateTime: Optional[datetime] = None
     eventStartDateTime: datetime
     eventEndDateTime: datetime
     location: Optional[str] = ""
@@ -29,29 +29,29 @@ class Event(BaseModel):
     description: Optional[str] = ""
     capacity: Optional[int] = 0
     price: Optional[float] = 0.00
-    status: Optional[str]= ""
-    helpers: Optional[list]= None
+    status: Optional[str] = ""
+    helpers: Optional[list] = None
+
 
 class EventInvitation(BaseModel):
-    eventId : int 
+    eventId: int
     inviteId: int
-    ownerId : int
+    ownerId: int
     sentDate: datetime
     invitationResponse: Optional[str] = ""
     textResponse: Optional[str] = ""
     RespondDate: Optional[datetime] = None
-    Notified :  Optional[int] = 0
+    Notified:  Optional[int] = 0
+
 
 class Task(BaseModel):
-    eventId : int
+    eventId: int
     taskId: int
     taskName: Optional[str] = ""
-    assignedTo : Optional[User] = None
+    assignedTo: Optional[User] = None
     startTime: Optional[datetime] = None
     endTime: Optional[datetime] = None
-    notes : Optional[str] = ""
-    taskStatus : Optional[str] = ""
+    notes: Optional[str] = ""
+    taskStatus: Optional[str] = ""
     description: Optional[str] = ""
-    creationDate : datetime
-
-
+    creationDate: datetime
